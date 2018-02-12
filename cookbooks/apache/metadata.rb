d0ca373a9b77e40040cc7bd369dbe7dbb33ca823
Meg <user@megburglin1.mylabserver.com>
2018-02-12 20:23:30 +0000

    Adding apache cookbook

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,8 @@
+name 'apache'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
